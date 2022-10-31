87e7e2ebd3975e082d224293a58ece6e35bb9645
Alya Putri 71210802 <112394868+Alyaputrijuliabate@users.noreply.github.com>
2022-10-31 19:58:38 +0700

    Add files via upload

---
+++ UG8_71210802.py
@@ -0,0 +1,80 @@
+class NodePelanggan:
+    def __init__(self, namaPelanggan):
+        self._namaPelanggan = namaPelanggan
+     
+    def getNamaPelanggan(self):
+        return self._namaPelanggan
+    
+    def setnamaPelanggan(self, namaPelangganBaru):
+        self._namaPelanggan = namaPelangganBaru
+        
+
+class Kasir:
+    DEFAULT_CAPACITY = 3 #isi sesuai dengan ketentuan soal
+    def __init__(self): #konstruktor
+       self._data = []
+       self._size = Kasir.DEFAULT_CAPACITY
+       self._front = 0
+       
+    def __len__(self): #mengembalikan ukuran Queue
+        return len(self._data)
+
+    def is_empty(self): #mengecek apakah Queue kosong ?
+        return len(self._data) == 0
+
+    def dequeue(self): #menghapus data paling depan (front)
+        data = self._data[0]
+        self._data.remove(data)
+        print(f"### Pelanggan {data} Selesai Membayar ### \n")
+
+    def enqueue(self, namaPelanggan): #menambah data ke list
+        self._data.append(namaPelanggan)
+
+    def resize(self, cap): #mengubah ukuran queue pada list
+        print(f"### Melakukan Resize ### \n")
+        self._size = self._size * cap
+    
+    def printAll(self): #menampilkan daftar pelanggan dalam sebuah kasir
+        
+        if len(self._data) > self._size:
+                self.resize(2)
+        print("=== Kasir ===")
+        c = len(self._data)-1
+        n = 1
+        for i in range(0, (self._size)):            
+            if i <= c:
+                print(n,".",self._data[i], end=' ')
+                print()
+            else:
+                print(n,". Kosong")
+            n += 1
+                
+        # else:
+        #     n = 1
+        #     for i in range(0, (self._capacity)):
+        #         print(n,".",self._data[i], end=' ')
+        #         n += 1
+        #         print()
+            
+        print()
+
+# test case program
+
+tempatKasir = Kasir()
+tempatKasir.enqueue("Haniif")
+tempatKasir.enqueue("Sindu")
+tempatKasir.enqueue("Dedi")
+tempatKasir.printAll()
+
+tempatKasir.enqueue("Beatrix")
+tempatKasir.printAll()
+
+tempatKasir.dequeue()
+tempatKasir.printAll()
+
+tempatKasir.enqueue("Shalom")
+tempatKasir.printAll()
+
+tempatKasir.dequeue()
+tempatKasir.printAll()
+
